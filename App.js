f8f6564bedc104c0a154a05a6da0cd2879cc11b3
NicollasDileo06 <nicgato2013@gmail.com>
2020-11-12 13:33:30 -0300

    helloworld

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+
+  const logar = () => {
+    alert('${email} ${senha}');
+  }
+     fetch('',{
+        method : 'POST',
+        body :{
+          email : email,
+          senha : senha
+        },
+        headers : {
+          'content-type' : 'application/json',
+        }
+
+     })
+     .then(response => response.json())
+     .then(data => alert(data))
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+
+      <Text>Email</Text>
+      <TextInput
+      style={{ height: 40, width : 200, borderColor: 'black', backgroundColor : "white", borderWidth: 1 }}
+      onChangeText={text => setEmail
+        (text)}
+      value={email}
+    />
+    <Text>Senha</Text>
+    <TextInput
+      style={{ height: 40, width : 200, borderColor: 'black', backgroundColor : "white", borderWidth: 1 }}
+      onChangeText={text => setSenha
+        (text)}
+      value={senha}
+    />
+    <Button
+  onPress={logar}
+  title="Logar"
+  color="#841584"
+  accessibilityLabel="Logar"
+/>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
